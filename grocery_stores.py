0fdd599e79e6db27fea392cf744dc551e56d9b37
Ismael Robles-Razzaq <ismaelrobles-razzaq@Ismaels-MacBook-Pro.local>
2024-02-28 17:16:17 -0600

    updated geojson data

--- grocery_stores.py
+++ grocery_stores.py
@@ -3,19 +3,17 @@ import json
 import geopy
 import time
 from geopy.geocoders import Nominatim
+import math
 
 
 
-api_key = ''#get key from github secrets
+api_key = 'AIzaSyC9lZ1X8kG2xjqw_1f9QU7dMLqeBUjW3kA'#get key from github secrets
 gmaps = googlemaps.Client(key=api_key)
 
 # Specify the location (Chicago) and the type of place (grocery_store)
-#geolocator = Nominatim(user_agent="geoapiExercises")
-#location = geolocator.geocode("Chicago, IL")
-
-#latitude, longitude = location.latitude, location.longitude
-latitude = 41.881832
-longitude = -87.623177
+#Southeastern most corner of Chicago, subtract 
+latitude = 41.64501235143132
+longitude = -87.52476251009567
 location = (latitude, longitude)
 
 place_type = 'grocery_or_supermarket'
@@ -23,7 +21,7 @@ place_type = 'grocery_or_supermarket'
 # Perform a Places API request to get grocery store locations
 
 def get_places_in_grid(gmaps, location, radius, place_type):
-    results = gmaps.places_nearby(location=location, radius=4000, type=place_type)
+    results = gmaps.places_nearby(location=location, radius=2000, type=place_type)
     grocery_stores = []
     while results.get('results'):
         for place in results['results']:
@@ -39,22 +37,23 @@ def get_places_in_grid(gmaps, location, radius, place_type):
         if 'next_page_token' in results:
             next_page_token = results['next_page_token']
             time.sleep(2)
-            results = gmaps.places_nearby(location=f"{latitude},{longitude}", radius=4000, type=place_type, page_token=next_page_token)
+            results = gmaps.places_nearby(location=f"{latitude},{longitude}", radius=2000, type=place_type, page_token=next_page_token)
         else:
             break
     return grocery_stores
     
 
-grid_size = 10  # Number of rows and columns in the grid
-radius_per_cell = 4000  # Radius for each grid cell in meters
+ns_size = 10  # Number of rows and columns in the grid
+ew_size = 7
+radius_per_cell = 2000  # Radius for each grid cell in meters
 
 # Perform Places API requests for each grid cell
 all_grocery_stores = []
-for i in range(grid_size):
-    for j in range(grid_size):
+for i in range(ns_size):
+    for j in range(ew_size):
         # Calculate the coordinates of the current grid cell
-        cell_latitude = latitude  + (4000/111139)+ i * (radius_per_cell / 111000)  # 1 degree of latitude is approximately 111,000 meters
-        cell_longitude = longitude + (4000/111139) +  j * (radius_per_cell / (111000 * abs((latitude + cell_latitude) / 2)))
+        cell_latitude = latitude  + i * (2 * radius_per_cell / 111000)  # 1 degree of latitude is approximately 111,000 meters
+        cell_longitude = longitude -  j * (2 * radius_per_cell / (111000.0 * abs(math.cos(math.radians(cell_latitude)))))
 
         # Form the location string for the Places API request
         cell_location = f"{cell_latitude},{cell_longitude}"
@@ -63,9 +62,10 @@ for i in range(grid_size):
         grocery_stores_in_cell = get_places_in_grid(gmaps, location=cell_location, radius=radius_per_cell, place_type='grocery_or_supermarket')
 
         all_grocery_stores.extend(grocery_stores_in_cell)
+    print(f"cell latitude: {cell_latitude}")
 
 # Save the data as a .json file
-with open('grocery_stores_chicago2.json', 'w') as json_file:
+with open('grocery_stores_chicago3.json', 'w') as json_file:
     json.dump(all_grocery_stores, json_file, indent=2)
 
 
